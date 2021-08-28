01f3ee43b6fdd314291c0fd3f674cdb2395f99be
janajolovic <72345853+janajolovic@users.noreply.github.com>
2021-08-28 23:19:02 +0200

    some changes

--- todoApp.js
+++ todoApp.js
@@ -3,13 +3,11 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 const filterOption = document.querySelector(".filter-todo");
 
-//Event Listeners
 document.addEventListener("DOMContentLoaded", getTodos);
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteTodo);
 filterOption.addEventListener("click", filterTodo);
 
-//Functions
 
 function addTodo(e) {
   //Prevent natural behaviour
